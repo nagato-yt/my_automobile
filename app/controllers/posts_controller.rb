7dfb4d85885eee1781e24056bfa2e6be2cf10cbf
nagato-yt <nagatoyutoyuto@gmail.com>
2023-05-03 21:53:57 +0900

    post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,29 @@
 class PostsController < ApplicationController
   def new
+    @post= Post.new
   end
 
   def index
+    @posts= Post.all
   end
 
   def edit
+  end
+  
+  def create
+    @post= Post.new(post_params)
+    @post.save
+    redirect to posts_path
+  end
+  
+  def destroy
+    @post.destroy
+    redirect to posts_path
+  end
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'posts/new'
-  get 'posts/index'
-  get 'posts/edit'
   root  'homes#top'
+  devise_for :users
   get 'homes/about'
+  resources :posts
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
